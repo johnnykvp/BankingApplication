104dfe81675f1bc5329508fb5f7935ee51859b61
Johnny Pham <jkvp@uw.edu>
2019-11-21 00:30:46 -0800

    Initial Commit

---
+++ ass5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ass5
+//
+//  Created by Johnny Pham on 11/21/19.
+//  Copyright © 2019 Johnny Pham. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
